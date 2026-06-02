e467f968240c7db0e4049a999ff249190cc4b577
ksoft <ksoftlabsinternational@gmail.com>
2026-06-02 08:04:28 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+//variables
+
+let age = 25;
+let price = 109.99;
+let gpa = 3.4;
+
+//output using template literals - allows us to embed expressions inside string literals, using backticks (`) and ${} syntax
+console.log(`You are ${age} years old`);
+console.log(`The price is $${price}`);
+console.log(`Your GPA is ${gpa}`);
+
